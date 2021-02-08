f84762a5c1084955808d4c22e361e64dd3355a69
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2021-02-09 02:13:33 +0800

    add test

--- app.go
+++ app.go
@@ -4,6 +4,29 @@ package main
 
 import "fmt"
 
+// variables and datatypes
+func variables() {
+	var age1 int = 25
+	var name1 string = "gilbert"
+	fmt.Println(name1, ": ", age1)
+
+	age2 := 25
+	name2 := "gilbert2"
+	fmt.Println(name2, ": ", age2)
+
+	var age3, name3 = 25, "gilbert3"
+	fmt.Println(name3, ": ", age3)
+
+	fmt.Println("Enter Your age:")
+	fmt.Scanln(&age3)
+
+	fmt.Println("Enter Your name:")
+	fmt.Scanln(&name3)
+
+	fmt.Println("So you are ", name3, "and your age is ", age3)
+}
+
 func main() {
 	fmt.Println("Hello mr. ??")
+	variables()
 }
